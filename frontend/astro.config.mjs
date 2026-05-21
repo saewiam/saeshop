e37dfd47666146ba7315bc1f1d92ec3d5020d099
Alex Ruiz <me@ruizalex.com>
2026-05-21 06:52:33 -0700

    Cloudflare Workers runtime for frontend

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,20 +4,5 @@ import { defineConfig } from 'astro/config'
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  integrations: [
-      {
-          name: 'dev-server-dynamic-render',
-          hooks: {
-              'astro:config:setup': (config) => {
-                  if (config.command === 'dev') {
-                      config.updateConfig({
-                          output: 'server',
-                      })
-                  }
-              },
-          },
-      },
-  ],
-
-  adapter: cloudflare(),
+    adapter: cloudflare(),
 })
