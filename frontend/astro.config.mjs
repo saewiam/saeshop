8e324c9eab8dd5463b63b72024f0f11e995059e2
Alex Ruiz <me@ruizalex.com>
2026-05-25 18:49:23 -0300

    Remove React integration

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,31 +3,29 @@ import { defineConfig, envField } from 'astro/config'
 
 import cloudflare from '@astrojs/cloudflare'
 
-import react from '@astrojs/react';
-
 export default defineConfig({
-  site: process.env.FRONTEND_SITE_URL || 'http://saeshop.localhost',
+    site: process.env.FRONTEND_SITE_URL || 'http://saeshop.localhost',
 
-  env: {
-      schema: {
-          STRIPE_SECRET_KEY: envField.string({
-              context: 'server',
-              access: 'secret',
-              optional: false,
-          }),
-          STRIPE_PUBLISHABLE_KEY: envField.string({
-              context: 'client',
-              access: 'public',
-              optional: false,
-          }),
-          FRONTEND_SITE_URL: envField.string({
-              context: 'client',
-              access: 'public',
-              optional: true,
-          }),
-      },
-  },
+    env: {
+        schema: {
+            STRIPE_SECRET_KEY: envField.string({
+                context: 'server',
+                access: 'secret',
+                optional: false,
+            }),
+            STRIPE_PUBLISHABLE_KEY: envField.string({
+                context: 'client',
+                access: 'public',
+                optional: false,
+            }),
+            FRONTEND_SITE_URL: envField.string({
+                context: 'client',
+                access: 'public',
+                optional: true,
+            }),
+        },
+    },
 
-  adapter: cloudflare(),
-  integrations: [react()],
+    adapter: cloudflare(),
 })
+
